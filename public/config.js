faaef2c23035914e1e24268ddb962210cd9fe913
orderly-guoqing <guoqing@orderly.network>
2026-09-13 01:51:16 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -43,5 +43,5 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ANALYTICS_CONFIG": "{\"version\":1,\"provider\":\"ga4\",\"measurementId\":\"G-LFVKJWSX52\"}",
   "VITE_SYMBOL_LIST": "",
   "VITE_RESTRICTED_REGIONS": "",
-  "VITE_WHITELISTED_IPS": "0"
+  "VITE_WHITELISTED_IPS": ""
 };
