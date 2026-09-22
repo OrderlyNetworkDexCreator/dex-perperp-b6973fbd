105be66b95b2bbad454260f015acfcc16a6c1dd9
orderly-guoqing <guoqing@orderly.network>
2026-09-22 10:47:36 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Trade crypto and real-world asset perpetuals on PerPerp. Deep omnichain liquidity, self-custody, and transparent fees.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Campaigns",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
   "VITE_CUSTOM_MENUS": "Learn,http://perperp.trade/masterclass;20X,http://perperp.trade/leverage;Refer,http://perperp.trade/refer;Buy,http://perperp.trade/buy",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
