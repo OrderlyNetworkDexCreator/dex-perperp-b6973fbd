1fc3d1453abf872c1c52405582d16e6381912da2
orderly-guoqing <guoqing@orderly.network>
2026-09-25 13:24:56 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -20,7 +20,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
-  "VITE_CUSTOM_MENUS": "Learn,http://perperp.trade/masterclass;20X,http://perperp.trade/leverage;Refer,http://perperp.trade/refer;Buy,http://perperp.trade/buy",
+  "VITE_CUSTOM_MENUS": "Learn,http://perperp.trade/masterclass;20X,http://perperp.trade/leverage;Refer,http://perperp.trade/refer;Buy,http://perperp.trade/buy;Home,http://perperp.trade",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/perperpdex",
